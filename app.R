533fb50ac4f11445a5862be566ee7e10f2d35b7e
Andrew Vancil <72818699+andrew-vancil@users.noreply.github.com>
2024-12-23 10:54:31 -0500

    pins rework part 1

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(bslib)
 library(gt)
-library(googlesheets4)
+
 
 # d <- tibble::tibble(
 #     id = as.character(seq(1,5,1)),
@@ -20,40 +20,24 @@ library(googlesheets4)
     
     d_all <- pins::pin_read(salt_board,
                         'vanug6_117748@cchmc.org/food_service_staff_listing')
+
     
-    d_all <- d_all |> 
-        tidyr::unite("eid_name", c(eid, name), remove = FALSE, sep = "-")
+    
 }
 
 ui <- page_navbar(
     title = "Attendance Tracking Dashboard",
     sidebar = sidebar(
         shinyWidgets::pickerInput(
-            inputId = "id_name",
+            inputId = "eid_name",
             choices = d_all$eid_name,
             multiple = FALSE,
             label = "Select employee:",
             options = list(
-                title = "nothing selected",
+                title = "nobody selected",
                 `live-search` = TRUE
             )
         ),
-        
-        # selectInput(
-        #     inputId = "employee_id",
-        #     label = "Employee ID:",
-        #     choices = d_all$eid,
-        #     selectize = TRUE,
-        #     selected = NULL
-        # ),
-        # 
-        # selectInput(
-        #     inputId = "name",
-        #     label = "Employee name:",
-        #     choices = d_all$name,
-        #     selectize = TRUE,
-        #     selected = NULL
-        # ),
         
 
         selectInput(
@@ -100,20 +84,15 @@ ui <- page_navbar(
         ),
         
         selectizeInput(
-            inputId = "supervisor",
-            label = "Supervisor: ",
-            choices = d$manager_name
+            inputId = "manager",
+            label = "Manager: ",
+            choices = d_all$manager_name
         ),
         
         actionButton(
             inputId = "add_row",
-            label = "Enter"
-        ),
-        hr(),
-        actionButton(
-            inputId = "save",
-            icon = icon("save"),
-            label = "Save data"
+            label = "Enter",
+            style = "background-color: #BECFB2;"
         ),
         
         width = 350
@@ -123,10 +102,32 @@ ui <- page_navbar(
     nav_panel("Occurrence table",
                   layout_column_wrap(
                       width = 1/2,
-                      card(gt_output("table")),
-                      card(gt_output("tracker"))
+                      card(
+                          card_header("Staff listing"),
+                          gt_output("table")
+                      ),
+                      
+                      card(
+                          card_header("New entries"),
+                          gt_output("new_rows"),
+                          actionButton(
+                              inputId = "save",
+                              icon = icon("save"),
+                              label = "Save data",
+                              style = "background-color: #BECFB2;"
+                          )),
+                      
+                      card(
+                          card_header("Existing entries"),
+                          gt_output("existing_rows")
+                      ),
+                      
+                      card(
+                          card_header("Point counter"),
+                          gt_output("point_counter"))
+                          
                   )
-              ),
+    ),
     nav_panel("Insights",
               card())
 )
@@ -134,24 +135,36 @@ ui <- page_navbar(
 server <- function(input, output, session) {
     
     d <- reactiveVal({
-        d
+        d_all
     })
+    
+    d_track <- reactiveVal({
+        
+        d_track <- pins::pin_read(
+            salt_board,
+            'vanug6_117748@cchmc.org/food_service_attendance_tracking_dataset')
+        
+        d_track$date <- as.Date(d_track$date, format = "%m-%d-%Y")
+        
+        as.data.frame(d_track)
+    })
+    
+    
 
     observeEvent(input$add_row, {
         
-        d <- d() |> 
+        d_track <- d_track() |> 
             dplyr::add_row(
-                id = input$employee_id,
-                name = input$name,
+                eid_name = input$eid_name,
                 type = input$type,
                 reason = input$reason,
                 notes = input$notes,
                 date = input$date,
                 grace = input$grace,
-                supervisor = input$supervisor
+                manager_name = input$manager
             )
         
-        d(d)
+        d_track(d_track)
 
 
     })
@@ -159,22 +172,62 @@ server <- function(input, output, session) {
     output$table <- render_gt({
         
         d() |> 
+            dplyr::select(-c(eid, name)) |> 
+            gt() |> 
+            cols_label(
+                eid_name = "Employee",
+                total_length_of_service = "Total tenure",
+                current_length_of_service = "Current tenure",
+                cost_center_number = "Cost center number",
+                cost_center_name = "Cost center name",
+                manager_name = "Manger",
+                work_space = "Location"
+            ) |> 
+            opt_interactive(
+                use_search = TRUE,
+                page_size_default = 5
+            )
+        
+    })
+    
+    output$new_rows <- render_gt({
+        
+        req(input$add_row)
+        
+        d_track() |> 
+            dplyr::filter(!is.na(type)) |> 
             gt() |>
             cols_label(
-                id = "Employee ID",
-                name = "Name",
+                eid_name = "Employee", 
                 type = "Type",
                 reason = "Call-off or\nTardy Reason",
                 notes = "Notes",
                 date = "Date",
                 grace = "Grace given?",
-                supervisor = "Supervisor"
+                manager_name = "Manager"
             )
 
     })
     
-    output$tracker <- render_gt({
+    output$existing_rows <- render_gt({
         
+        pins::pin_read(
+            salt_board,
+            'vanug6_117748@cchmc.org/food_service_attendance_tracking_dataset') |> 
+            dplyr::filter(!is.na(type)) |> 
+            gt() |>
+            cols_label(
+                eid_name = "Employee", 
+                type = "Type",
+                reason = "Call-off or\nTardy Reason",
+                notes = "Notes",
+                date = "Date",
+                grace = "Grace given?",
+                manager_name = "Manager"
+            )
+    })
+    
+    output$counter <- render_gt({
 
         d() |>
             dplyr::mutate(point_day = ifelse(grace == FALSE, 
@@ -204,28 +257,50 @@ server <- function(input, output, session) {
     
     observeEvent(input$save, {
         
-        sheet_write(
-            d(),
-            ss,
-            "occurrences_data")
-        
-        sheet_write(
-            d() |>
-                tidyr::unite("name", c(first_name, last_name), remove = TRUE, sep = " ")  |>
-                dplyr::mutate(point_day = ifelse(grace == FALSE, 
-                                                 ifelse(type == "tardy", .25, 1),
-                                                 0)) |> 
-                dplyr::mutate(total_points = sum(point_day),
-                              tot_num_occurrences = dplyr::n(), .by = id) |> 
-                dplyr::select(id, name, total_points, tot_num_occurrences) |>
-                dplyr::slice_tail(n = 1, by = id),
-            ss,
-            "tracker"
+        pins::pin_write(
+            salt_board,
+            d_track() |> 
+                dplyr::mutate(date = as.Date(date, format = "%m-%d-%Y")),
+            "food_service_attendance_tracking_dataset", 
+            type = "csv"
         )
+        
+        output$existing_rows <- render_gt({
+
+            pins::pin_read(
+                salt_board,
+                'vanug6_117748@cchmc.org/food_service_attendance_tracking_dataset') |>
+                dplyr::filter(!is.na(type)) |>
+                gt() |>
+                cols_label(
+                    eid_name = "Employee",
+                    type = "Type",
+                    reason = "Call-off or\nTardy Reason",
+                    notes = "Notes",
+                    date = "Date",
+                    grace = "Grace given?",
+                    manager_name = "Manager"
+                )
+        })
+        
+        
+        # sheet_write(
+        #     d() |>
+        #         tidyr::unite("name", c(first_name, last_name), remove = TRUE, sep = " ")  |>
+        #         dplyr::mutate(point_day = ifelse(grace == FALSE, 
+        #                                          ifelse(type == "tardy", .25, 1),
+        #                                          0)) |> 
+        #         dplyr::mutate(total_points = sum(point_day),
+        #                       tot_num_occurrences = dplyr::n(), .by = id) |> 
+        #         dplyr::select(id, name, total_points, tot_num_occurrences) |>
+        #         dplyr::slice_tail(n = 1, by = id),
+        #     ss,
+        #     "tracker"
+        # )
         
         showModal(modalDialog(
             title = "Success!",
-            tags$p("Data successfully saved to drive ", tags$a(href = ss$spreadsheet_url,
+            tags$p("Data successfully saved to drive ", tags$a(href = "https://salt.cchmc.org/content/6dba6a34-a9c7-4ac1-aa86-1e51f36f166b",
                                                                "here",
                                                                target = "_blank"))
         ))
